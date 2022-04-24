e2f4f379caaf40bd1c89401d105d43fd4514c08b
15tpopat <97695260+15tpopat@users.noreply.github.com>
2022-04-24 23:01:24 +0100

    Sixth commit

--- core/utils.py
+++ core/utils.py
@@ -12,8 +12,34 @@ from scipy.stats import entropy
 
 import gym_anytrading
 
-env = gym.make('stocks-v0')
-print(env)
+# env = gym.make('stocks-v0')
+# print(env)
+
+from gym.envs.registration import register
+from copy import deepcopy
+
+from . import datasets
+
+
+register(
+    id='forex-v0',
+    entry_point='gym_anytrading.envs:ForexEnv',
+    kwargs={
+        'df': deepcopy(datasets.FOREX_EURUSD_1H_ASK),
+        'window_size': 24,
+        'frame_bound': (24, len(datasets.FOREX_EURUSD_1H_ASK))
+    }
+)
+
+register(
+    id='stocks-v0',
+    entry_point='gym_anytrading.envs:StocksEnv',
+    kwargs={
+        'df': deepcopy(datasets.STOCKS_GOOGL),
+        'window_size': 30,
+        'frame_bound': (30, len(datasets.STOCKS_GOOGL))
+    }
+)
 
 class LinearSchedule(object):
     def __init__(self, schedule_timesteps, final_p, initial_p=1.0):
