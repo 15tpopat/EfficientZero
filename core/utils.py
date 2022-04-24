f9273995f188396fb4d6787eba9d3a2cd425ee2d
15tpopat <97695260+15tpopat@users.noreply.github.com>
2022-04-24 23:47:35 +0100

    Tenth commit

--- core/utils.py
+++ core/utils.py
@@ -26,9 +26,9 @@ register(
     id='stocks-v0',
     entry_point='gym_anytrading.envs:StocksEnv',
     kwargs={
-        'df': deepcopy(datasets.STOCKS_GOOGL),
+        'df': deepcopy(agdatasets.STOCKS_GOOGL),
         'window_size': 30,
-        'frame_bound': (30, len(datasets.STOCKS_GOOGL))
+        'frame_bound': (30, len(agdatasets.STOCKS_GOOGL))
     }
 )
 
