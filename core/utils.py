bb0c198375e5dcbd6ab35b1c6e5ac4a65cd4c16a
15tpopat <97695260+15tpopat@users.noreply.github.com>
2022-04-24 23:43:22 +0100

    Seventh commit

--- core/utils.py
+++ core/utils.py
@@ -18,18 +18,9 @@ import gym_anytrading
 from gym.envs.registration import register
 from copy import deepcopy
 
-from . import datasets
+from agdatasets import datasets
 
 
-register(
-    id='forex-v0',
-    entry_point='gym_anytrading.envs:ForexEnv',
-    kwargs={
-        'df': deepcopy(datasets.FOREX_EURUSD_1H_ASK),
-        'window_size': 24,
-        'frame_bound': (24, len(datasets.FOREX_EURUSD_1H_ASK))
-    }
-)
 
 register(
     id='stocks-v0',
@@ -258,7 +249,10 @@ def make_game(env_id, df=None, skip=4, max_episode_steps=None):
     max_episode_steps: int
         max moves for an episode
     """
-    env = gym.make(env_id, df=df)
+    if df is None:
+        env = gym.make(env_id)
+    else:
+        env = gym.make(env_id, df=df)
     assert 'NoFrameskip' in env.spec.id
     env = NoopResetEnv(env, noop_max=30)
     env = MaxAndSkipEnv(env, skip=skip)
